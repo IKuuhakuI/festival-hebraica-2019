4730d80bb6e558ac3fe10a723d24d258fe845329
Luiz-Sama <themax285gamer@gmail.com>
2019-11-03 20:52:26 -0300

    Altera o design do Kapaim

--- projeto_danca_android/app/src/main/java/com/ort/luiz/projeto_danca/PedidoActivity.java
+++ projeto_danca_android/app/src/main/java/com/ort/luiz/projeto_danca/PedidoActivity.java
@@ -1,13 +1,24 @@
 package com.ort.luiz.projeto_danca;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class PedidoActivity extends AppCompatActivity {
+
+    Button btnVoltarEventos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedido);
+
+        btnVoltarEventos = findViewById(R.id.btnVoltarEventosId);
+
+        btnVoltarEventos.setOnClickListener((V)->{
+            btnVoltarEventos.setBackgroundResource(R.color.White);
+            startActivity(new Intent(this, MainActivity.class));
+        });
     }
 }
